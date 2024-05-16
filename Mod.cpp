d84e064c5576304ca64e0b4d7f5155335c1417d2
Cynichill <logancampbell@live.co.uk>
2024-05-17 00:29:48 +0100

    Added json output, removed unneeded sections and dependancies

--- Mod.cpp
+++ Mod.cpp
@@ -4,27 +4,23 @@
 #include <detours.h>
 #include <thread>
 #include <string>
-#include <curl/curl.h>
+#include <fstream>
 #include <nlohmann/json.hpp>
 #include "Diva.h"
 
 // MegaMix+ addresses
 
-const uint64_t DivaScoreBaseAddress = 0x00000001412EF568;
-const uint64_t DivaScoreCompletionRateAddress = 0x00000001412EF634;
-const uint64_t DivaScoreWorstCounterAddress = 0x00000001416E2D40; // For whatever reason the "worst" counter is stored separately from the rest of the hit counters
 const uint64_t DivaScoreGradeAddress = 0x00000001416E2D00;
 const uint64_t DivaCurrentPVTitleAddress = 0x00000001412EF228;
 const uint64_t DivaCurrentPVIdAddress = 0x00000001412C2340;
 
 // Non-SongLimitPatch 1.02
-//const uint64_t DivaCurrentPVDifficultyAddress = 0x00000001412B634C;
+const uint64_t DivaCurrentPVDifficultyAddress = 0x00000001412B634C;
 
 // SongLimitPatch 1.02 ONLY
-const uint64_t DivaCurrentPVDifficultyAddress = 0x00000001423157AC;
+//const uint64_t DivaCurrentPVDifficultyAddress = 0x00000001423157AC;
 
-const std::string ApiEndpoint = "https://eo45wawywebpo55.m.pipedream.net";
-const std::string ConfigFileName = "config.toml";
+const std::string OutputFileName = "mods/ArchipelagoMod/results.json";
 
 bool consoleEnabled = true;
 
@@ -33,112 +29,41 @@ void* DivaScoreTrigger = sigScan(
     "xxxx?xxxx?xxxx?xxxxxxxxxxxxxxxxxxx????xxxxxxxxxxxx?????xx????"
 );
 
-void curlOperation(std::string resultsString)
-{
-    CURL* curl;
-    CURLcode res;
+void writeToFile(const std::string& resultsString) {
 
-    if (GetConsoleWindow()) {
-        if (GetConsoleOutputCP() != CP_UTF8) {
-            SetConsoleOutputCP(CP_UTF8);
-        }
-        consoleEnabled = freopen("CONOUT$", "w", stdout) != NULL;
+    // Write the JSON to a file
+    std::ofstream outputFile(OutputFileName);
+    if (outputFile.is_open()) {
+        outputFile << resultsString;
+        outputFile.close();
     }
-
-    if (consoleEnabled)
-        printf(resultsString.c_str());
-
-    curl_global_init(CURL_GLOBAL_ALL);
-
-    curl = curl_easy_init();
-
-    if (curl) {
-        curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "POST");
-
-        const char* endpoint = ApiEndpoint.c_str();
-        curl_easy_setopt(curl, CURLOPT_URL, endpoint);
-
-        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-        curl_easy_setopt(curl, CURLOPT_DEFAULT_PROTOCOL, "https");
-        curl_easy_setopt(curl, CURLOPT_CAINFO, "mods/ShareDiva/curl-ca-bundle.crt");
-
-        struct curl_slist* headers = NULL;
-        headers = curl_slist_append(headers, "Content-Type: application/json");
-        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
-
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, resultsString.length());
-
-        // Add the results body to the request
-        const char* data = resultsString.c_str();
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data);
-
-        // Send request, read the result, print any errors or confirm successful send
-        res = curl_easy_perform(curl);
-
-        curl_easy_cleanup(curl);
+    else {
+        if (consoleEnabled)
+            printf("Failed to open the file for writing.\n");
     }
-
-    curl_global_cleanup();
 }
 
 HOOK(int, __fastcall, _PrintResult, DivaScoreTrigger, int a1) {
 
-    DIVA_SCORE DivaScore = *(DIVA_SCORE*)DivaScoreBaseAddress;
-    int DivaScoreWorst = *(int*)DivaScoreWorstCounterAddress;
-    DIVA_STAT DivaStat = *(DIVA_STAT*)DivaScoreCompletionRateAddress;
     std::string& DivaTitle = *(std::string*)DivaCurrentPVTitleAddress;
     DIVA_PV_ID DivaPVId = *(DIVA_PV_ID*)DivaCurrentPVIdAddress;
     DIVA_DIFFICULTY DivaDif = *(_DIVA_DIFFICULTY*)DivaCurrentPVDifficultyAddress;
     DIVA_GRADE DivaGrade = *(_DIVA_GRADE*)DivaScoreGradeAddress;
-
-    // Client-side processing of whether or not to send the results to ShareDiva bot
-    bool postScore = true;
-
-    switch (DivaDif)
-    {
-        case Normal:
-            if (DivaStat.CompletionRate >= 50.0F)
-                postScore = true;
-            break;
-        case Hard:
-            if (DivaStat.CompletionRate >= 55.0F)
-                postScore = true;
-            break;
-        case Extreme:
-        case ExExtreme:
-            if (DivaStat.CompletionRate >= 65.0F)
-                postScore = true;
-            break;
-        case Easy:
-        default:
-            break;
-    }
-
-    if (!postScore)
-        return original_PrintResult(a1);
 
     // Create JSON with all results that will be sent to the bot
     nlohmann::json results = {
         {"pvId", DivaPVId.Id},
         {"pvName", DivaTitle},
         {"pvDifficulty", DivaDif},
-        {"totalScore", DivaScore.TotalScore},
-        {"completionRate", DivaStat.CompletionRate},
         {"scoreGrade", DivaGrade},
-        {"combo", DivaScore.Combo},
-        {"cool", DivaScore.Cool},
-        {"fine", DivaScore.Fine},
-        {"safe", DivaScore.Safe},
-        {"sad", DivaScore.Sad},
-        {"worst", DivaScoreWorst}
     };
 
     // Dump JSON into a string
     std::string resultsString = results.dump();
 
-    // Detach a thread that will be sending the result so the game doesn't hang
-    std::thread curlThread(curlOperation, resultsString);
-    curlThread.detach();
+    // Detach a thread that will be writing the result so the game doesn't hang
+    std::thread fileWriteThread(writeToFile, resultsString);
+    fileWriteThread.detach();
 
     return original_PrintResult(a1);
 };
@@ -147,6 +72,6 @@ extern "C"
 {
     void __declspec(dllexport) Init()
     {
-        INSTALL_HOOK(_PrintResult);               
-    }    
+        INSTALL_HOOK(_PrintResult);
+    }
 }
