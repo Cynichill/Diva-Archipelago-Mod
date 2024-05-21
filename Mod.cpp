066e3f672143a91030d7acdf8378a4cba951f0bb
Cynichill <logancampbell@live.co.uk>
2024-05-21 16:27:46 +0100

    Updated to stop 'fake' wins

--- Mod.cpp
+++ Mod.cpp
@@ -13,6 +13,7 @@
 const uint64_t DivaCurrentPVTitleAddress = 0x00000001412EF228;
 const uint64_t DivaCurrentPVIdAddress = 0x00000001412C2340;
 const uint64_t DivaScoreGradeAddress = 0x00000001416E2D00;
+const uint64_t DivaScoreCompletionRateAddress = 0x00000001412EF634;
 
 // Non-SongLimitPatch 1.02
 const uint64_t DivaCurrentPVDifficultyAddress = 0x00000001412B634C;
@@ -28,6 +29,13 @@ void* DivaScoreTrigger = sigScan(
     "\x48\x89\x5C\x24\x00\x48\x89\x74\x24\x00\x48\x89\x7C\x24\x00\x55\x41\x54\x41\x55\x41\x56\x41\x57\x48\x8B\xEC\x48\x83\xEC\x60\x48\x8B\x05\x00\x00\x00\x00\x48\x33\xC4\x48\x89\x45\xF8\x48\x8B\xF9\x80\xB9\x00\x00\x00\x00\x00\x0F\x85\x00\x00\x00\x00",
     "xxxx?xxxx?xxxx?xxxxxxxxxxxxxxxxxxx????xxxxxxxxxxxx?????xx????"
 );
+
+//Difficulty thresholds
+float thresholdEasy = 30.0;
+float thresholdNormal = 50.0;
+float thresholdHard = 60.0;
+float thresholdExtreme = 70.0;
+float thresholdExExtreme = 70.0;
 
 void writeToFile(const nlohmann::json& results) {
 
@@ -49,13 +57,52 @@ HOOK(int, __fastcall, _PrintResult, DivaScoreTrigger, int a1) {
     DIVA_PV_ID DivaPVId = *(DIVA_PV_ID*)DivaCurrentPVIdAddress;
     DIVA_DIFFICULTY DivaDif = *(_DIVA_DIFFICULTY*)DivaCurrentPVDifficultyAddress;
     DIVA_GRADE DivaGrade = *(_DIVA_GRADE*)DivaScoreGradeAddress;
+    DIVA_STAT DivaStat = *(DIVA_STAT*)DivaScoreCompletionRateAddress;
+
+    int finalGrade = int(DivaGrade);
+    int difficulty = int(DivaDif);
+    float percentageEarned = float(DivaStat.CompletionRate);
+
+    //If % earned is less than threshold, fail song
+    if (finalGrade == 2)
+    {
+        switch (difficulty) 
+        {
+            case 0: // Easy
+                if (percentageEarned < thresholdEasy) 
+                {
+                    finalGrade = 1;
+                }
+                break;
+            case 1: // Normal
+                if (percentageEarned < thresholdNormal) {
+                    finalGrade = 1;
+                }
+                break;
+            case 2: // Hard
+                if (percentageEarned < thresholdHard) {
+                    finalGrade = 1;
+                }
+                break;
+            case 3: // Extreme
+                if (percentageEarned < thresholdExtreme) {
+                    finalGrade = 1;
+                }
+                break;
+            case 4: // ExExtreme
+                if (percentageEarned < thresholdExExtreme) {
+                    finalGrade = 1;
+                }
+                break;
+        }
+    }
 
     // Create JSON with all results that will be sent to the bot
     nlohmann::json results = {
         {"pvId", DivaPVId.Id},
         {"pvName", DivaTitle},
         {"pvDifficulty", DivaDif},
-        {"scoreGrade", DivaGrade},
+        {"scoreGrade", finalGrade},
     };
 
     // Detach a thread that will be writing the result so the game doesn't hang
