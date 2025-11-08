fde67c020e3036c9cacb68f24e233fd699e182cb
RePod <RePod@users.noreply.github.com>
2025-11-07 21:38:18 -0500

    APLogger: potential file log

--- APLogger.cpp
+++ APLogger.cpp
@@ -1,16 +1,29 @@
 #include "APLogger.h"
 #include <windows.h>
 #include <iostream>
+#include <fstream>
+#include <stdarg.h>
 
 void APLogger::print(const char* const fmt, ...)
 {
+    va_list args;
+    va_start(args, fmt);
+
     if (GetConsoleWindow() != NULL) {
         printf("[Archipelago] ");
-        va_list args;
-        va_start(args, fmt);
         vprintf(fmt, args);
-        va_end(args);
     }
+
+    // Log to file
+    /*FILE* log = fopen("AP.txt", "a");
+
+    if (log != NULL) {
+        fprintf(log, "[Archipelago] ");
+        vfprintf(log, fmt, args);
+        fclose(log);
+    }*/
+
+    va_end(args);
 
     // Something is causing problems with the output reaching the console under Wine.
     // Probably the PreInit in dllmainDebug. "Temporary" long term fix.
