410a7e663bc85e190fc378585dc9a6967564b8f5
RePod <RePod@users.noreply.github.com>
2026-03-30 12:18:40 -0400

    Mod: merge framework.h into pch.h

--- pch.h
+++ pch.h
@@ -8,8 +8,9 @@
 #define PCH_H
 
 // add headers that you want to pre-compile here
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+#include <windows.h>
 #include "APLogger.h"
-#include "framework.h"
 #include "Helpers.h"
 #include <filesystem>
 #include <fstream>
